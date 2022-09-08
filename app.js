70b6b9bdd431c8e374a8f2f9c4a2a02c8cd8ce72
Yuchen <chengcheng0805@gmail.com>
2022-09-09 00:30:38 +0800

    router finish S3-U78

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const Todo = require('./models/todo')
 require('dotenv').config()
 const MY_ENV = process.env.MY_ENV
+const router = require('./routes')
 
 //connect mongoose
 mongoose.connect(MY_ENV, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,63 +24,19 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //use bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 //index show the todo
-app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
-})
 
-app.get('/todos/news', (req, res) => {
-  res.render('new')
-})
+app.use(router)
+// app.get('/', (req, res) => {
+//   Todo.find()
+//     .lean()
+//     .sort({ _id: 'asc' })
+//     .then(todos => res.render('index', { todos }))
+//     .catch(error => console.error(error))
+// })
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  // const todo = new Todo({ name: name })
-  // return todo.save().then(() => res.redirect('/'))
-  //   .catch(error => console.log('error'))
-  return Todo.create({ name: name }).then(() => res.redirect('/'))
-    .catch(error => console.log('error'))
-})
 
-app.get('/todos/:id', (req, res) => {
-
-  const id = req.params.id
-  return Todo.findById(id).lean().then(todo => res.render('detail', { todo }))
-    .catch(error => console.log('error'))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id).lean().then(todo => res.render('edit', { todo }))
-    .catch(error => console.log('error'))
-})
-
-app.post('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id).then(todo => {
-    todo.name = name
-    todo.isDone = isDone === 'on'
-    return todo.save()
-  })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => { console.log(error) })
-
-})
-
-app.post('/todos/:id/delete', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id).then(todo => {
-    todo.remove()
-  })
-    .then(() => res.redirect('/'))
-    .catch(error => { console.log(error) })
-})
 
 
 app.listen(3000, () => {
